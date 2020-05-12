87f81bf29aee51ccff6133a3effa8ab19056534a
Rafael Alberdi <vallejo@demos-deutschland.de>
2020-05-12 17:35:05 +0200

    added new type Image2seq to admin page

--- app/api/admin.py
+++ app/api/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from .models import Label, Document, Project
 from .models import Role, RoleMapping
-from .models import DocumentAnnotation, SequenceAnnotation, Seq2seqAnnotation
-from .models import TextClassificationProject, SequenceLabelingProject, Seq2seqProject
+from .models import DocumentAnnotation, SequenceAnnotation, Seq2seqAnnotation, Image2seqAnnotation
+from .models import TextClassificationProject, SequenceLabelingProject, Seq2seqProject, Image2seqProject
 
 
 class LabelAdmin(admin.ModelAdmin):
@@ -42,6 +42,12 @@ class Seq2seqAnnotationAdmin(admin.ModelAdmin):
     search_fields = ('document',)
 
 
+class Img2seqAnnotationAdmin(admin.ModelAdmin):
+    list_display = ('document', 'text', 'user')
+    ordering = ('document',)
+    search_fields = ('document',)
+
+
 class RoleAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     ordering = ('name',)
@@ -57,11 +63,13 @@ class RoleMappingAdmin(admin.ModelAdmin):
 admin.site.register(DocumentAnnotation, DocumentAnnotationAdmin)
 admin.site.register(SequenceAnnotation, SequenceAnnotationAdmin)
 admin.site.register(Seq2seqAnnotation, Seq2seqAnnotationAdmin)
+admin.site.register(Image2seqAnnotation, Img2seqAnnotationAdmin)
 admin.site.register(Label, LabelAdmin)
 admin.site.register(Document, DocumentAdmin)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(TextClassificationProject, ProjectAdmin)
 admin.site.register(SequenceLabelingProject, ProjectAdmin)
 admin.site.register(Seq2seqProject, ProjectAdmin)
+admin.site.register(Image2seqProject, ProjectAdmin)
 admin.site.register(Role, RoleAdmin)
 admin.site.register(RoleMapping, RoleMappingAdmin)
